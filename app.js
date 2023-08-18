63f181724b49e366d200e2aaed66ed2393ac9a4c
unikdahal <unikdahal03@gmail.com>
2023-08-18 21:25:44 +0530

    Laying the Foundation

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+// const heading = React.createElement("h1", {id: heading},"Namaste React");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const heading = <h1 className="heading">Namaste Javascript</h1>;
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
